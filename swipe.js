0b1375dc87b63613f1eeaba59bf11e081932d748
Paco Rampas <pacorampas@gmail.com>
2015-02-16 12:22:52 +0100

    added counter

--- swipe.js
+++ swipe.js
@@ -7,9 +7,11 @@ window.onload = function(){
 	var timeOutId = null;
 	var maxStep = null;
 
-	var element = document.getElementById("wrapper-panels");
+	var element = document.getElementById('wrapper-panels');
+	var counter = document.getElementById('counter');
 	element.style.width = widthWrapperPanels(element)+'px';
 	maxStep = element.querySelectorAll('.panel').length;
+
 	
 	element.addEventListener("touchstart", function(event){
 		element.classList.remove('transition');
@@ -23,35 +25,30 @@ window.onload = function(){
 		var diffTouchPos = newTouchPos - touchPos;
 		touchPos = newTouchPos;
 		despl = despl + diffTouchPos;
-		console.log(despl);
 		element.style.transform = 'translateX('+despl+'px)';
 	}, false);
 
 	element.addEventListener("touchend", function(event){
 		var timeEnd = event.timeStamp; 
 		var timeDiff = timeEnd - timeStart;
-		console.log(timeEnd +' - '+ timeStart+' = '+timeDiff);
 		fastSwipe = false;
 		if(timeDiff < 250){
 			fastSwipe = true;
+		}
 
-		} 
 		var goToNextStep = nextStep(despl, step, maxStep, fastSwipe);
-		console.log(goToNextStep);
 		
 		touchPos = goToNextStep.start;
 		despl = goToNextStep.start;
 		step = goToNextStep.step;
 		element.classList.add('transition');
 		element.style.transform = 'translateX('+despl+'px)';
+		if(counter){
+			element.dataset.step = step;
+			counter.textContent = step;
+		}
 	}, false);
 
-	/*element.addEventListener("touchcancel", function(event){
-		console.log('cancel');
-	}, false);
-	element.addEventListener("touchleave", function(event){
-		console.log('leave');
-	}, false);*/
 	element.addEventListener('transitionend', function(){
 		element.classList.remove('transition');
 	}, false);
@@ -74,22 +71,17 @@ function nextStep(despl, step, maxStep, fastSwipe){
 	var width = window.innerWidth;
 	var relativeDespl = Math.abs(despl - (start*-1));
 	var percentage = relativeDespl*100/width;
-	console.log('Start: '+start+' - End: '+end+' - Width: '+width+' - Despl '+despl);
 	var relativeDespl = Math.abs(despl - start);
 	var percentage = relativeDespl*100/width;
-	console.log('RelativeDespl: '+relativeDespl+' - Perc '+percentage);
-	console.log('rightDesplacement');
-	//the direcction is to negative values, for that is necessary to change some values to negative
+	
 	if(despl < start ){
 		//right
-		console.log(percentage +'> 60 && '+ maxStep +' < '+ step+1);
-		if( (fastSwipe || percentage > 60) && maxStep > step+1){
+		if( (fastSwipe || percentage > 35) && maxStep > step+1){
 			return {start: end, step: step+1};
 		}
 	}else{
 		//left
-		console.log(percentage +'> 60');
-		if( (fastSwipe || percentage > 60) && step > 0){
+		if( (fastSwipe || percentage > 35) && step > 0){
 			return {start: start+width, step: step-1};
 		}
 	}
